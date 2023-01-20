f54bef3fb71cd73ebb0a1c967eeab971938ec59f
Mohammad Nafisi <nafisidev@gmail.com>
2023-01-20 22:20:44 +0800

    The Twitter API call works

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,42 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css';
+import { React, useState, useEffect } from 'react';
 
-export default function Home() {
+const default_endpoint = 'https://api.twitter.com/2/users/:id/followers';
+
+export async function getServerSideProps() {
+
+  var axios = require('axios');
+
+  var config = {
+    method: 'get',
+    url: 'https://api.twitter.com/2/users/1264069561869049856/followers',
+    headers: { 
+      'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANIplQEAAAAAnCU9cx3hmSwQdoGbed3%2Bc6uOeIY%3D2wj9XP0PoJz8ZYdtzQHWJ6pe6Ocm6IR3XDeQsIZRyn4gkFFzxz', 
+      'Cookie': 'guest_id=v1%3A167404021070772330'
+    }
+  };
+
+  axios(config)
+  .then(function (response) {
+    const data = JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  //const data = JSON.stringify(response.data);
+  return{
+    props:{
+      data
+    }
+  }
+}
+
+export default function Home({ data}) {
+  
+  console.log("WoWwwwwwwwwwwwwwww", data);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+    <div>
+      
     </div>
   )
 }
